1a683bf9ea5fd902788558097c4268bb9633c667
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-01-03 00:57:13 -0600

    CMakeLists.txt: Customize for our project style
    * My nomlib & ttcards repo will be using this concept as a starting template.

--- include/hello/hello.h
+++
@@ -1,7 +0,0 @@
-#ifndef HELLO_HELLO_H_INC
-#define HELLO_HELLO_H_INC
-
-#include <SDL.h>
-#include <SDL_opengles2.h>
-
-#endif
---
+++ src/main.mm
@@ -0,0 +1,172 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2013 Jeffrey Carpenter. All rights reserved.
+//
+
+#include <iostream>
+//#include <cstdio>
+//#include <cstdlib>
+#include <stdbool.h>
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+static bool gpause = false;
+
+static const int screen_width = 320;
+static const int screen_height = 480;
+
+static SDL_Window *win = NULL;
+static SDL_Renderer *renderer = NULL;
+
+int main(int argc, char * argv[])
+{
+  int x = 0, y = 0; // mouse coords
+/*
+  int dx = 0, dy = 0;
+  unsigned char state;
+*/
+  std::cout << "\n" << "Entry of " << __func__ << "\n";
+
+  if ( SDL_Init ( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf ( "Error: %s\n", SDL_GetError() );
+    exit ( -1 );
+  }
+  atexit ( SDL_Quit );
+
+  if ( IMG_Init ( IMG_INIT_PNG ) != IMG_INIT_PNG )
+  {
+    std::cout << "\n" << IMG_GetError() << "\n";
+    exit ( -1 );
+  }
+  atexit ( IMG_Quit );
+
+  if ( TTF_Init () == -1 )
+  {
+    std::cout << "\n" << TTF_GetError() << "\n";
+    exit ( -1 );
+  }
+  atexit ( TTF_Quit );
+
+  // Hide the status bar
+  win = SDL_CreateWindow ( NULL, 0, 0, 0, 0,
+                          SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS );
+  if ( !win )
+  {
+    printf ( "Error: %s\n", SDL_GetError() );
+    exit ( 1 );
+  }
+
+  renderer = SDL_CreateRenderer(win,-1,0);
+
+  bool running = true;
+  SDL_Event event;
+
+  SDL_Rect rect;
+  rect.x = 0;
+  rect.y = 0;
+  rect.w = screen_width / 2;
+  rect.h = screen_height / 2;
+
+  SDL_Rect rect2;
+  rect2.x = 192;
+  rect2.y = 128;
+  rect2.w = 128;
+  rect2.h = 128;
+
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF );
+  SDL_RenderClear(renderer);
+
+  while ( running )
+  {
+    while ( SDL_PollEvent ( &event ) )
+    {
+      switch ( event.type )
+      {
+        default: /* ignore */ break;
+
+        case SDL_QUIT:
+        {
+          running = false;
+          break;
+        }
+
+        case SDL_MOUSEBUTTONDOWN:
+        {
+          switch ( event.button.button )
+          {
+            case SDL_BUTTON_LEFT:
+            {
+              x = event.button.x;
+              y = event.button.y;
+              //state = SDL_GetMouseState(&x, &y);  /* get its location */
+              //SDL_GetRelativeMouseState(&dx, &dy);        /* find how much the mouse moved */
+              //if ( state & SDL_BUTTON_LMASK ) /* is the mouse (touch) down? */
+              //{
+              SDL_Rect mouse;
+              mouse.x = x;
+              mouse.y = y;
+              mouse.w = 64;
+              mouse.h = 64;
+
+              SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+              SDL_SetRenderDrawColor ( renderer, 0x0, 0xFF, 0x0, 0xFF );
+              SDL_RenderFillRect ( renderer, &mouse );
+              std::cout << "\nLEFT\n";
+              break;
+            } // SDL_BUTTON_LEFT
+
+            case SDL_BUTTON_RIGHT:
+            {
+              std::cout << "\nRIGHT\n";
+              break;
+            }
+          } // event.button.button
+        } // MOUSEBUTTON_DOWN
+/*
+
+          state = SDL_GetMouseState(&x, &y);
+          //SDL_GetRelativeMouseState(&dx, &dy);
+
+          if ( state & SDL_BUTTON_RMASK )
+          {
+            SDL_Rect mouse2;
+            mouse2.x = x;
+            mouse2.y = y;
+            mouse2.w = 64;
+            mouse2.h = 64;
+
+            SDL_SetRenderDrawColor ( renderer, 0x0, 0x0, 0xFF, 0xFF );
+            SDL_RenderFillRect ( renderer, &mouse2 );
+            std::cout << "\nRight\n";
+          }
+          break;
+        }
+*/
+      } // event.type
+    } // events
+
+    if ( gpause != true )
+    {
+      SDL_RenderPresent ( renderer );
+
+      SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_ADD);
+      SDL_SetRenderDrawColor ( renderer, 0xFF, 0x0, 0xFF, 0xFF );
+      SDL_RenderFillRect ( renderer, &rect );
+
+      SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_ADD);
+      SDL_SetRenderDrawColor ( renderer, 0xFF, 0x0, 0x0, 0xFF );
+      SDL_RenderFillRect ( renderer, &rect2 );
+    }
+  }
+
+  SDL_DestroyWindow(win);
+
+  std::cout << "\n" << "Exit of " << __func__ << "\n";
+
+  return 0;
+}

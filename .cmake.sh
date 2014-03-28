0982a727b1725f590e822d6fd359f9907a3434e5
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-03-28 15:55:18 -0500

    Relocate cmake.sh from proj root to dist dir

--- .cmake.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-
-cmake -GXcode NOM_BUILD_IOS=on ..
---
+++ dist/cmake.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# This helper script must be ran from the project's build directory
+#
+
+cmake -GXcode NOM_BUILD_IOS=on ..
